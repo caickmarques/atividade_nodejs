1a0e6571ae65e43cc1ed78dd2441b7605cdf6170
caickmarques <caick_marques@hotmail.com>
2020-06-23 20:33:26 -0300

    Ramificado codigo

--- covid.js
+++ covid.js
@@ -2,22 +2,25 @@ let readline = require('readline-sync');
 
 console.log('Vamos nos preteger da COVID-19?');
 
-let mascara = readline.question('Voce esta usando mascara ao sair de casa? (Sim/Nao): ').toUpperCase();
+let mascara = readline.question('Você esta usando mascara ao sair de casa? (Sim/Nao): ').toUpperCase();
 
-while (mascara != "SIM" && mascara != "NAO") {
-    console.log('Favor digitar uma resposta valida!');
-    mascara = readline.question('Voce esta usando mascara ao sair de casa? (Sim/Nao): ');
+while (mascara != "SIM" && mascara != "NAO" && mascara != "NÃO") {
+    console.log('Favor digitar uma resposta válida!');
+    mascara = readline.question('Você esta usando mascara ao sair de casa? (Sim/Nao): ').toUpperCase();
 }
 
-let lavar_maos = readline.question('Voce esta lavando as maos frequentemente? (Sim/Nao): ').toUpperCase();
+let lavar_maos = readline.question('Você esta lavando as maos frequentemente? (Sim/Nao): ').toUpperCase();
 
-while (lavar_maos != "SIM" && lavar_maos != "NAO") {
-    console.log('Favor digitar uma resposta valida!');
-    lavar_maos = readline.question('Voce esta lavando as maos frequentemente? (Sim/Nao): ');
+while (lavar_maos != "SIM" && lavar_maos != "NAO" && lavar_maos != "NÃO") {
+    console.log('Favor digitar uma resposta válida!');
+    lavar_maos = readline.question('Você esta lavando as maos frequentemente? (Sim/Nao): ').toUpperCase();
 }
 
-if (mascara == 'SIM' && lavar_maos == 'SIM') {
-    console.log('Parabens! Voce esta se protegendo corretamente!');
-} else {
-    console.log('Que feio hein?! Então comece a lavar as maos frequentemente e a utilizar mascara ao sair de casa!');
-}
+lavar_maos == 'SIM' && mascara == 'SIM' ? console.log("Parabéns") : console.log("Use mascara e lave as mãos!!!");
+
+
+// if (mascara == 'SIM' && lavar_maos == 'SIM') {
+//     console.log('Parabens! Voce esta se protegendo corretamente!');
+// } else {
+//     console.log('Então comece a lavar as maos frequentemente e a utilizar mascara ao sair de casa!');
+// }
